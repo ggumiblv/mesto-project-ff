d6b2e8dcd473a3871ecd1b9656e4a66d4d5ff25b
ggumiblv <gulnazhakimova70@gmail.com>
2024-01-29 15:17:10 +0100

    first

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,35 @@
 // @todo: Темплейт карточки
 
+const cardTemplate = document.querySelector("#card-template").content; 
+
 // @todo: DOM узлы
+
+const placesList = document.querySelector(".places__list"); 
 
 // @todo: Функция создания карточки
 
+function createCard(cardDetails) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true); 
+
+  cardElement.querySelector(".card__image").src = cardDetails.link;
+  cardElement.querySelector(".card__title").textContent = cardDetails.name;
+
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+    deleteButton.addEventListener("click", () => {
+      deleteCard(cardElement);
+    });
+
+  return cardElement;
+}
+
 // @todo: Функция удаления карточки
 
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
+
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach((item) => {
+  placesList.append(createCard(item));
+});
